4240dc376e9509ac1a93a0ab20e17081209146ad
mirsharofdoja <mirsharoftursunaliyev@gmail.com>
2023-06-21 07:38:04 +0500

    task

---
+++ basicfunction.py
@@ -0,0 +1,21 @@
+def func1():
+    return 0
+def func2():
+    return "Hello World"
+def func3():
+    return "codeschooluz"
+def func4():
+    return int(1)
+def func5():
+    return float(1.5)
+def func6():
+    return "Hello World"
+def func7(a):
+    return a
+def func8(a):
+    return a+1
+def func9(a):
+    return a-1
+def func10(a):
+    return -a
+print(type(func10(5)))
