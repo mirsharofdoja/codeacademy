13b3b2edf42035f31a3bb6d7c63d8091f2d48f2f
mirsharofdoja <mirsharoftursunaliyev@gmail.com>
2023-06-26 16:32:57 +0500

    vazifa

---
+++ String02.py
@@ -0,0 +1,9 @@
+def main():
+    """
+    Return a string consisting only of uppercase letters.
+    Args:
+        None
+    Returns:
+        str: return answer.
+    """
+    return "PYTHONUZ"
---
+++ String10.py
@@ -0,0 +1,10 @@
+def main(x,y):
+    """
+    Given two integers x, y return the "(x+y)*2={answer}" string.
+    Args:
+        x: int
+        y: int
+    Returns:
+        str: return answer.
+    """
+    return f'({x}+{y})*2={x*y}'
